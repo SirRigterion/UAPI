90fc25b20e1edc23fca05debec59087c010ab3ae
Sir Rigterion <art371qwe@gmail.com>
2025-04-04 12:14:31 +0300

    Add auto update project

--- alembic/env.py
+++ alembic/env.py
@@ -3,18 +3,13 @@ from alembic import context
 from src.db.models import Base
 from src.core.config import settings
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Устанавливаем URL базы данных из settings без интерполяции configparser
-config.attributes['sqlalchemy.url'] = settings.DATABASE_URL
 
-# add your model's MetaData object here for 'autogenerate' support
+config.attributes['sqlalchemy.url'] = settings.DATABASE_URL
 target_metadata = Base.metadata
 
 def run_migrations_offline() -> None:
@@ -32,13 +27,12 @@ def run_migrations_offline() -> None:
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode with async engine."""
-    from src.db.database import engine  # Импортируем асинхронный движок из проекта
+    from src.db.database import engine
     
     connectable = engine
 
     async def do_run_migrations():
         async with connectable.connect() as connection:
-            # Выполняем миграции синхронно внутри run_sync
             await connection.run_sync(do_configure_and_run)
 
     def do_configure_and_run(sync_conn):
